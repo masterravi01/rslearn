b2fb108018287fe2264b6d42472f0c141b38dcf3
Ravi Parmar <ravi.parmar@simulas.in>
2024-05-25 10:54:34 +0000

    db connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
-import express from "express";
-const app = express();
 import dotenv from "dotenv";
 dotenv.config();
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+const port = process.env.PORT || 8000;
 
-const port = process.env.PORT || 4000;
+import connectDB from "./db/index.js";
+
+connectDB()
+
+// ; (async () => {
+//   try {
+//     // Ensure you have imported and configured mongoose and app before this block
+//     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     console.log(`\n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`);
+
+//     // Error handling for the app
+//     app.on('error', (error) => {
+//       console.log("ERROR:", error);
+//       throw error;
+//     });
+
+
+//     // Start the Express server
+//     app.listen(port, () => {
+//       console.log(`App listening on port ${port}`);
+//     });
+
+//   } catch (error) {
+//     console.error('ERROR:', error);
+//     throw error;
+//   }
+// })();
+
+
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -18,5 +52,5 @@ app.get("/gogi", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App is listening on port ${port}`);
 });
