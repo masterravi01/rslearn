6139534cc409afc085e8d7d85b2fa342516e36e9
Ravi Parmar <ravi.parmar@simulas.in>
2024-05-25 07:51:35 +0000

    add prettier settings

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import express from 'express';
-const app = express()
-import dotenv from 'dotenv';
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
 dotenv.config();
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/rslearn', (req, res) => {
-    res.send('<h1>Hello , Welcome to rs learn</h1>')
-})
+app.get("/rslearn", (req, res) => {
+  res.send("<h1>Hello , Welcome to rs learn</h1>");
+});
 
-app.get('/gogi', (req, res) => {
-    res.send('<h3>My name is master</h3>')
-})
+app.get("/gogi", (req, res) => {
+  res.send("<h3>My name is master</h3>");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
