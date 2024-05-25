c4f35a7808d13e3f71b78eca6d1fb034b5418a08
Ravi Parmar <ravi.parmar@simulas.in>
2024-05-25 11:45:58 +0000

    db connection update & utils code added

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
 const app = express();
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+
 const port = process.env.PORT || 8000;
 
 import connectDB from "./db/index.js";
 
 connectDB()
-
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR:", error);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`App is listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection failed !!!", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-app.get("/rslearn", (req, res) => {
-  res.send("<h1>Hello , Welcome to rs learn</h1>");
-});
-
-app.get("/gogi", (req, res) => {
-  res.send("<h3>My name is master</h3>");
-});
-
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
 });
