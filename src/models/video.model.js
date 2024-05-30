70f4d966a96cde45e76f41168d077cd92836f8b6
Ravi Parmar <ravi.parmar@simulas.in>
2024-05-30 19:07:41 +0100

    cloudinary & multer setup

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,47 +1,47 @@
 import mongoose, { Schema } from "mongoose";
 
 const videoSchema = new Schema(
-    {
-        videoFile: {
-            type: String,
-            required: true
-        },
-        thumbnail: {
-            type: String,
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String,
-            required: true
-        },
-        videoFile: {
-            type: String,
-            required: true
-        },
-        duration: {
-            type: Number,
-            required: true
-        },
-        views: {
-            type: Number,
-            default: 0
-        },
-        isPublished: {
-            type: Boolean,
-            default: true
-        },
-        owner: {
-            type: Schema.Types.ObjectId,
-            ref: "User"
-        }
+  {
+    videoFile: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
-)
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const Video = mongoose.model("Video", videoSchema)
+export const Video = mongoose.model("Video", videoSchema);
