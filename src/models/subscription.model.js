740892591e6f03e48c3e4b637ef313c2e88bc775
Ravi Parmar <ravi.parmar@simulas.in>
2024-06-10 07:07:52 +0000

    remaining routes ,controller added

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -13,4 +13,4 @@ const subscriptionSchema = new Schema({
 
 
 
-export const Subsciption = mongoose.model("Subsciption", subscriptionSchema)
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
