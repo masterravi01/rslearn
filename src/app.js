3d4c9362ea04dea78398045746508e207cddf0a6
Ravi Parmar <ravi.parmar@simulas.in>
2024-10-21 10:11:15 +0000

    midddleware added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import { errorHandler } from "./middlewares/error.middlewares.js";
+import { morganMiddleware } from "./logger/morgan.logger.js";
+import path from "path";
 
 const app = express();
 
@@ -15,14 +17,24 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: false, limit: "16kb" }));
+app.use(express.static("public"));
 app.use(cookieParser());
+app.use(morganMiddleware);
 
+//routes import
+import indexRouter from "./routes/index.routes.js";
 
+//routes declaration
+app.use("/api/v1", indexRouter);
 
-
-
+// Handling preflight requests
+// preflight requests sent by the browser to determine whether the actual request (e.g., a GET or POST request) is safe to send.
 app.options("*", cors());
 
+app.set("view engine", "ejs");
+app.set("views", path.join(path.resolve(), "src", "views"));
 
+app.use(errorHandler);
 
 export { app };
